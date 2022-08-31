49862854333715e5abfd2f432c1a19b2b791c418
Spoetnik1 <37273190+Spoetnik1@users.noreply.github.com>
2022-08-31 20:55:00 +0200

    added waitress to requirements.txt and use waitress for a WSGI server to serve the webpage

--- run.py
+++ run.py
@@ -3,5 +3,7 @@ import os
 
 
 if __name__ == '__main__':
+    from waitress import serve
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    serve(app, host="0.0.0.0", port=port)
+    # app.run(debug=True, host='0.0.0.0', port=port)
